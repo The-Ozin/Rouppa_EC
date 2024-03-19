87da66ddb9fc436da1a1af51ae642831f8520166
The-Ozin <127219688+The-Ozin@users.noreply.github.com>
2024-03-18 22:41:25 -0300

    CSS universal, recursividade e cadastro de produto
    CSS universal para a barra de navegação, recursividade em algumas páginas e cadastro de produto 90 % completo. apenas a imagem não está carregando.

--- Rouppa_EC/brecho/brecho.js
+++ Rouppa_EC/brecho/brecho.js
@@ -2,8 +2,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     const produtosContainer = document.getElementById("produtosContainer");
 
-    // Carregar produtos quando a página é carregada
-    carregarProdutos();
+    // Verificar se o elemento existe antes de adicionar o event listener
+    if (produtosContainer) {
+        carregarProdutos();
+    } else {
+        console.error("Elemento 'produtosContainer' não encontrado na página.");
+    }
 
     function carregarProdutos() {
         // Recuperar a lista de produtos do armazenamento local
@@ -17,14 +21,26 @@ document.addEventListener("DOMContentLoaded", function() {
             const preco = produto.preco.toFixed(2);
             const produtoHTML = `
                 <div class="col-md-4">
-                    <!-- Estrutura do card do produto -->
-                    <p>Preço: R$ ${preco}</p>
+                    <div class="card mb-4 box-shadow">
+                        <img class="card-img-top" src="${produto.imagem}" alt="Imagem do produto">
+                        <div class="card-body">
+                            <h5 class="card-title">${produto.nome}</h5>
+                            <p class="card-text">Categoria: ${produto.categoria}</p>
+                            <p class="card-text">Tamanho: ${produto.tamanho}</p>
+                            <p class="card-text">Gênero: ${produto.genero}</p>
+                            <p class="card-text">Descrição: ${produto.descricao}</p>
+                            <p class="card-text">Preço: R$ ${preco}</p>
+                        </div>
+                    </div>
                 </div>
             `;
             produtosContainer.innerHTML += produtoHTML;
         });
     }
 });
+
+
+
 
 
 
