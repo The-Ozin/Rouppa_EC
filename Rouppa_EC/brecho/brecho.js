03d05a224f0da33cca984d216c1914ee3a4166fd
The-Ozin <127219688+The-Ozin@users.noreply.github.com>
2024-03-20 14:57:08 -0300

    Correções no css
    redirecionamento de pastas e algumas correções no css.

--- Rouppa_EC/brecho/brecho.js
+++ Rouppa_EC/brecho/brecho.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function() {
         produtosContainer.innerHTML = "";
 
         // Iterar sobre os produtos e exibi-los
-        produtos.forEach(function(produto) {
+        produtos.forEach(function(produto, index) {
             const preco = produto.preco.toFixed(2);
             const produtoHTML = `
                 <div class="col-md-4">
@@ -30,14 +30,47 @@ document.addEventListener("DOMContentLoaded", function() {
                             <p class="card-text">Gênero: ${produto.genero}</p>
                             <p class="card-text">Descrição: ${produto.descricao}</p>
                             <p class="card-text">Preço: R$ ${preco}</p>
+                            <button type="button" class="btn btn-danger excluir-produto">Excluir</button>
                         </div>
                     </div>
                 </div>
             `;
             produtosContainer.innerHTML += produtoHTML;
         });
+
+        // Chamar a função para adicionar os botões de exclusão
+        adicionarBotoesExclusao();
+    }
+
+    // Adicionar botões de exclusão para cada produto
+    function adicionarBotoesExclusao() {
+        const botoesExclusao = document.querySelectorAll('.excluir-produto');
+        botoesExclusao.forEach((botao, index) => {
+            botao.addEventListener('click', () => {
+                // Remover o produto correspondente da lista
+                removerProduto(index);
+            });
+        });
+    }
+
+    // Remover produto da lista e atualizar o armazenamento local
+    function removerProduto(index) {
+        // Recuperar a lista de produtos do armazenamento local
+        const produtos = JSON.parse(localStorage.getItem('produtos')) || [];
+
+        // Remover o produto do índice especificado
+        produtos.splice(index, 1);
+
+        // Atualizar a lista de produtos no armazenamento local
+        localStorage.setItem('produtos', JSON.stringify(produtos));
+
+        // Recarregar a página para refletir as alterações
+        location.reload();
     }
 });
+
+
+
 
 
 
