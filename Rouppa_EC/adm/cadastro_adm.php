c5f8e76d45794fbae2b7fa710e4ddc34846565a1
Ítalo de Andrade <italolande@gmail.com>
2024-04-02 19:36:38 -0300

    crud cadastro

--- Rouppa_EC/adm/cadastro_adm.php
+++ Rouppa_EC/adm/cadastro_adm.php
@@ -1,18 +1,16 @@
 <?php
 include("../connect.php");
-$nome = $_POST["nome"];
-$email = $_POST["email"];
-$data_nsc = $_POST["data"];
+$id_adm = $_POST["id_adm"];
 $senha = $_POST["senha"];
 $cpf = $_POST["cpf"];
 
-$sql = "INSERT INTO usuario(cpf, nome, email, data_nascimento, senha) VALUES('$cpf', '$nome', '$email', '$data_nsc', '$senha')";
+$sql = "INSERT INTO adm(id_adm, cpf, senha) VALUES('$id_adm','$cpf', '$senha')";
 $result = $conn->query($sql);
 
 if ($result === TRUE) {
     ?>
     <script>
-        alert('Cliente cadastrado');
+        alert('adm cadastrado');
     </script>
     <?php
 } else {
