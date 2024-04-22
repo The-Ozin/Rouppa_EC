82589e627039fbc6a102a419abb1f00db6a47c02
Ítalo de Andrade <italolande@gmail.com>
2024-04-22 03:18:09 -0300

    adm page 2

--- Rouppa_EC/adm/adm_main.php
+++ Rouppa_EC/adm/adm_main.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="adm_main.css">
-    <link rel="stylesheet" href="/main1.css">
-    <title>adm page</title>
+    <title>adm pagina</title>
+<style>
+    ul{color: white;}
+</style>
 </head>
 <body>
 
@@ -23,36 +25,70 @@
     <button onclick="pagAdmLogin()">editar adm</button>
 </div>
 
-<h2>Lista de Usuários</h2>
-<ul>
-    <?php
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "Simba8!#";
-    $dbname = "";
+<div  class="listas scrollable" id="lista_usuario">
+    <h2 id="titulo1">Lista de Usuários</h2>
+    <ul>
+        <?php
+            include "../connect.php";
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+            $sql = "SELECT * FROM usuario";
+            $result = $conn->query($sql);
 
-    if ($conn->connect_error) {
-        die("Erro de conexão: " . $conn->connect_error);
-    }
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                    echo "<li>" . $row["nome"]. "    /    " .$row["cpf"]. "</li>";
+                }
+            } else {
+                echo "Nenhum usuário encontrado";
+            }
 
+        ?>
+    </ul>
+</div>
 
-    $sql = "SELECT * FROM usuarios";
-    $result = $conn->query($sql);
+<div class="listas scrollable" id="lista_loja">
+    <h2 id="titulo2">Lista de lojas</h2>
+    <ul>
+        <?php
+            include "../connect.php";
 
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            echo "<li>" . $row["nome"] . "</li>";
-        }
-    } else {
-        echo "Nenhum usuário encontrado";
-    }
+            $sql = "SELECT * FROM loja";
+            $result = $conn->query($sql);
 
-    $conn->close();
-    ?>
-</ul>
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                    echo "<li>" . $row["nome_loja"]."    /    ".$row["cnpj"] . "</li>";
+                }
+            } else {
+                echo "Nenhuma loja encontrada";
+            }
+
+        ?>
+    </ul>
+</div>
+
+<div class="listas scrollable" id="lista_adm">
+
+    <ul>
+        <h2 id="titulo3">Lista de adms</h2>
+        <?php
+            include "../connect.php";
+
+            $sql = "SELECT * FROM adm";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                    echo "<li>" . $row["id_adm"]."    /    ".$row["cpf"] . "</li>";
+                }
+            } else {
+                echo "Nenhum usuário encontrado";
+            }
+
+        ?>
+    </ul>
+</div>
 
 </body>
 </html>
