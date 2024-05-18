b29d90e5b0b60c3f3f8e31387866898aca5a2f32
TheoRavaglia <theo.ravaglia@pucpr.edu.br>
2024-05-17 22:20:32 -0300

    página adm
    adiconado a visualização de usuário e loja. e a exclusão dinãmica conforme o cadastro for adicionado. não faz sentido adm editar essas informações, pois elas devem ser feitas pelo usuário e lojas.

--- adm/adm.php
+++ adm/adm.php
@@ -1,3 +1,21 @@
+<?php 
+include('../connect.php');
+
+// Função para deletar usuário
+if (isset($_GET['delete_user_id'])) {
+    $userId = intval($_GET['delete_user_id']);
+    $query = "DELETE FROM usuarios WHERE id = $userId";
+    mysqli_query($conn, $query);
+}
+
+// Função para deletar loja
+if (isset($_GET['delete_store_id'])) {
+    $storeId = intval($_GET['delete_store_id']);
+    $query = "DELETE FROM lojas WHERE id = $storeId";
+    mysqli_query($conn, $query);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,60 +25,166 @@
     <link rel="stylesheet" href="../assets/style.css">
 
     <!-- Font Awesome -->
-    <link
-    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-    rel="stylesheet"
-    />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"/>
     <!-- Google Fonts -->
-    <link
-    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-    rel="stylesheet"
-    />
-
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet"/>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Display:ital,wght@0,100..900;1,100..900&family=Source+Serif+4:ital,opsz,wght@0,8..60,200..900;1,8..60,200..900&display=swap" rel="stylesheet">
 
     <!-- MDB -->
-    <link
-    href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.2.0/mdb.min.css"
-    rel="stylesheet"
-    />
-
-    <script src="
-    https://cdn.jsdelivr.net/npm/sweetalert2@11.10.8/dist/sweetalert2.all.min.js
-    "></script>
-
-        <script
-        type="text/javascript"
-        src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.2.0/mdb.umd.min.js"
-        
-        initMDB({ Input, Tab, Ripple });
-    ></script>
-
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.2.0/mdb.min.css" rel="stylesheet"/>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.8/dist/sweetalert2.all.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.2.0/mdb.umd.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    </head>
-    <body>
-        <a href="../welcome.php"><img src="../assets/images/logo1.jpg" alt="Rouppa"></a>
-        <h1>Admnistração da Rouppa</h1>
-    </body>
+
+    <style>
+        h1 {
+            color: rgb(215,90, 90);
+            text-align: center;
+            margin-top: -10vh;
+            font-weight: bold;
+        }
+        img {
+            width: 200px;
+            height: 200px;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            margin-top: 20vh;
+            transform: translateY(-50%);
+        }
+        .container {
+            margin-top: 20px;
+        }
+        .table-container {
+            margin: 20px auto;
+            width: 80%;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 10px;
+            border: 1px solid #ddd;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+        .btn {
+            margin: 5px;
+        }
+    </style>
+</head>
+<body>
+    <a href="../welcome.php"><img src="../assets/images/logo1.jpg" alt="Rouppa"></a>
+    <h1>Administração da Rouppa</h1>
+
+    <div class="container">
+        <div class="table-container">
+            <h2>Usuários</h2>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Email</th>
+                        <th>CPF</th>
+                        <th>Data de Nascimento</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $query = "SELECT * FROM usuario";
+                    $result = mysqli_query($conn, $query);
+
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr>";
+                            echo "<td>" . htmlspecialchars($row['nome']) . "</td>";
+                            echo "<td>" . htmlspecialchars($row['email']) . "</td>";
+                            echo "<td>" . htmlspecialchars($row['cpf']) . "</td>";
+                            echo "<td>" . htmlspecialchars($row['data_nascimento']) . "</td>";
+                            echo "<td><button class='btn btn-danger' onclick='deleteUser(" . $row['id'] . ")'>Excluir</button></td>";
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "<tr><td colspan='5'>Nenhum usuário encontrado</td></tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="table-container">
+            <h2>Lojas</h2>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>CNPJ</th>
+                        <th>Nome</th>
+                        <th>Endereço</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $query = "SELECT * FROM loja";
+                    $result = mysqli_query($conn, $query);
+
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr>";
+                            echo "<td>" . htmlspecialchars($row['cnpj']) . "</td>";
+                            echo "<td>" . htmlspecialchars($row['nome']) . "</td>";
+                            echo "<td>" . htmlspecialchars($row['endereco']) . "</td>";
+                            echo "<td><button class='btn btn-danger' onclick='deleteStore(" . $row['id'] . ")'>Excluir</button></td>";
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "<tr><td colspan='4'>Nenhuma loja encontrada</td></tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <script>
+        function deleteUser(userId) {
+            Swal.fire({
+                title: 'Tem certeza?',
+                text: "Você não poderá reverter isso!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sim, exclua!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = "?delete_user_id=" + userId;
+                }
+            });
+        }
+
+        function deleteStore(storeId) {
+            Swal.fire({
+                title: 'Tem certeza?',
+                text: "Você não poderá reverter isso!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sim, exclua!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = "?delete_store_id=" + storeId;
+                }
+            });
+        }
+    </script>
+</body>
 </html>
 
-<style>
-    h1 {
-        color: rgb(215,90, 90);
-        text-align: center;
-        margin-top: -10vh;
-        font-weight: bold;
-    }
-    img {
-        width: 200px;
-        height: 200px;
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-        margin-top: 20vh;
-        transform: translateY(-50%);
-    }
-
-</style>
